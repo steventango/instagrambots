6858057738e25486e662e60488be499433010f47
Steven Tang <18170455+steventango@users.noreply.github.com>
2021-09-17 10:17:25 -0600

    Improve getUsername

--- instalike.user.js
+++ instalike.user.js
@@ -1,63 +1,89 @@
 // ==UserScript==
 // @name         InstaLike
-// @namespace    https://github.com/steventango/instagrambots
-// @version      2.2.7
+// @version      2.4.0
 // @description  Like bot for Instagram
-// @author       Steven Tang
+// @author       Steven
 // @match        https://www.instagram.com
 // @require      https://steventang.tk/bot/steven.bot.js
 // ==/UserScript==
+const BLACK_LIST = ["USERNAME"];
+
+function getParent(like_element) {
+  let parent = like_element;
+  while (parent.nodeName != "ARTICLE") {
+    parent = parent.parentElement;
+  }
+  return parent;
+}
+
+function getUsername(like_element) {
+  const parent = getParent(like_element);
+  return parent.querySelector("header div:nth-child(2) a").innerText;
+};
 
 function instalike() {
-    var likeCount = 0;
-    var blackList = [
-        'USERNAME'
-    ];
-    var tLikes = localStorage.getItem('steven.bot.instalike.likes') || 0;
-    var nextTime = Math.random() * 10000 + 2000;
-    var likeElements = Array.from(document.querySelectorAll('article div section span button svg')).filter(e => e.getAttribute('aria-label') == 'Like' ? 1 : 0);
-    likeElements = likeElements.filter(v => blackList.indexOf(v.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText) === -1);
-    var notify, parent;
-    if (likeElements.length > 0) {
-        parent = likeElements[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-        var username = parent.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText;
-    }
-    if (parent) {
-        parent.scrollIntoView();
-        likeElements[0].parentNode.click();
+  let like_count = 0;
+  let total_likes = localStorage.getItem("steven.bot.instalike.likes") || 0;
+  let next_time = Math.random() * 10000 + 2000;
+  let like_elements = Array.from(
+    document.querySelectorAll("article div section span button svg")
+  ).filter((like_element) => (like_element.getAttribute("aria-label") == "Like" ? 1 : 0));
+  like_elements = like_elements.filter(
+    (like_element) => BLACK_LIST.indexOf(getUsername(like_element)) === -1
+  );
+  let parent, username;
+  if (like_elements.length > 0) {
+    parent = getParent(like_elements[0]);
+    username = getUsername(like_elements[0]);
+  }
+  if (parent) {
+    parent.scrollIntoView();
+    like_elements[0].parentNode.click();
 
-        if (Notification.permission === 'granted') {
-            notify = new Notification('Liked Post: ' + username, {
-                icon: 'https://www.instagram.com/static/images/ico/favicon-192.png/b407fa101800.png',
-            });
-            setTimeout(notify.close.bind(notify), 1500);
-            notify.onclick = function() {
-                window.focus();
-                this.close();
-            };
-        }
-        tLikes++;
-        localStorage.setItem('steven.bot.instalike.likes', tLikes);
-        likeCount++;
-        console.log('Liking Next Post in: ' + (nextTime / 1000) + ' seconds.');
-        setTimeout(instalike, nextTime);
-    } else if ((likeCount / document.querySelectorAll('article').length) * 100 > 75) {
-        console.log('Checking For More Posts in: ' + (nextTime / 1000) + ' seconds.');
-        window.scrollTo(0, document.body.scrollHeight);
-        setTimeout(instalike, nextTime + 2000);
-    } else {
-        console.log('No Photos to like.');
-        console.log('Total Like Count: ' + tLikes);
+    if (Notification.permission === "granted") {
+      const NOTIFY = new Notification("Liked Post: " + username, {
+        icon: "https://www.instagram.com/static/images/ico/favicon-192.png/b407fa101800.png",
+      });
+      setTimeout(NOTIFY.close.bind(NOTIFY), 1500);
+      NOTIFY.onclick = function () {
+        window.focus();
+        this.close();
+      };
     }
+    total_likes++;
+    localStorage.setItem("steven.bot.instalike.likes", total_likes);
+    like_count++;
+    console.log(`Liking next post in: ${next_time / 1000} s.`);
+    setTimeout(instalike, next_time);
+  } else if (
+    (like_count / document.querySelectorAll("article").length) * 100 >
+    75
+  ) {
+    console.log(`Checking for more posts in: ${next_time / 1000 + 2} s.`);
+    window.scrollTo(0, document.body.scrollHeight);
+    setTimeout(instalike, next_time + 2000);
+  } else {
+    console.log("No posts to like.");
+    console.log("Total like count: " + total_likes);
+  }
 }
-steven.bot(() => {
+
+steven.bot(
+  () => {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
-    if (Notification.permission !== 'denied' || Notification.permission !== 'granted') {
-        Notification.requestPermission();
+    if (
+      Notification.permission !== "denied" ||
+      Notification.permission !== "granted"
+    ) {
+      Notification.requestPermission();
     }
-}, instalike, 600000, {
-    name: 'InstaLike',
-    version: '2.2.7',
+  },
+  instalike,
+  900000,
+  {
+    name: "InstaLike",
+    version: "2.4.0",
     reload: true,
-    reload_url: 'https://www.instagram.com'
-});
+    reload_url: "https://www.instagram.com",
+  }
+);
